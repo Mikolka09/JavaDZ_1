1361400d5c4de4d27092b6e15480ec907f038985
Микола Матіос <mikolka09@gmail.com>
2022-09-05 21:40:23 +0200

    05.09.2022

--- src/DZ_9/WorkForDictionary.java
+++ src/DZ_9/WorkForDictionary.java
@@ -2,10 +2,7 @@ package DZ_9;
 
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.TreeMap;
+import java.util.*;
 
 public class WorkForDictionary implements Runnable {
 
@@ -23,9 +20,14 @@ public class WorkForDictionary implements Runnable {
 
         while (true) {
             System.out.println("\n\n\t\tMENU");
-            System.out.println("\t1. Начальный ввод данных\n\t2. Вывод слова и перпевод\n\t3. Работа с переводами слова" +
-                    "\n\t4. Работа со словом.\n\t5. Топ-10 самых популярных слов.\n\t6. Топ-10 самых непопулярных слов" +
-                    "\n\t7. Выход");
+            System.out.println("""
+                    \t1. Начальный ввод данных
+                    \t2. Вывод слова и перевод
+                    \t3. Работа с переводами слова
+                    \t4. Работа с иностранным словом
+                    \t5. Топ-10 самых популярных слов
+                    \t6. Топ-10 самых непопулярных слов
+                    \t7. Выход""");
             System.out.print("\nВаш выбор: ");
             int var = Integer.parseInt(scan.nextLine());
             switch (var) {
@@ -71,10 +73,94 @@ public class WorkForDictionary implements Runnable {
     }
 
     private void workForTranslateWorld() {
+        boolean bool = true;
+        while (bool)
+        {
+            System.out.println("\nРабота с переводами слова");
+            System.out.println("\n\n\t\tMENU");
+            System.out.println("""
+                \t1. Добавить перевод
+                \t2. Заменить перевод
+                \t3. Удалить перевод
+                \t4. Выход""");
+            System.out.print("\nВаш выбор: ");
+            int var = Integer.parseInt(scan.nextLine());
+            switch (var){
+                case 1 -> addTranslateWorld();
+                case 2 -> changeTranslateWorld();
+                case 3 -> deleteTranslateWorld();
+                default -> bool = false;
+            }
+        }
 
     }
 
+    private void addTranslateWorld(){
+        System.out.println("\nДобавить слово к переводу");
+        boolean bool = true;
+        while (bool)
+        {
+            System.out.print("\nВведите английское слово: ");
+            String str = scan.nextLine();
+            print(str, this.dict.dictionary.get(str));
+            System.out.print("\nВведите еще один вариант перевода: ");
+            String sl = scan.nextLine();
+            this.dict.dictionary.get(str).add(sl);
+            System.out.println("Слово добавлено!");
+            print(str, this.dict.dictionary.get(str));
+            System.out.print("\nХотите добавить еще одно слово перевода (1-Да, 2-Нет): ");
+            int res = Integer.parseInt(scan.nextLine());
+            switch (res) {
+                case 1 -> {
+                }
+                case 2 -> bool = false;
+                default -> throw new IllegalStateException("Unexpected value: " + res);
+            }
+        }
+
+    }
+
+    private void changeTranslateWorld(){
+
+    }
+
+    private void deleteTranslateWorld(){
+
+    }
+
+
+
     private void workForWorld() {
+        boolean bool = true;
+        while (bool)
+        {
+            System.out.println("\nРабота с иностранным словом");
+            System.out.println("\n\n\t\tMENU");
+            System.out.println("""
+                \t1. Добавить слово
+                \t2. Заменить заменить
+                \t3. Удалить слово
+                \t4. Выход""");
+            System.out.print("\nВаш выбор: ");
+            int var = Integer.parseInt(scan.nextLine());
+            switch (var){
+                case 1 -> addWorld();
+                case 2 -> changeWorld();
+                case 3 -> deleteWorld();
+                default -> bool = false;
+            }
+        }
+    }
+
+    private void addWorld(){
+
+    }
+
+    private void changeWorld(){
+
+    }
+
+    private void deleteWorld(){
 
     }
 
@@ -92,11 +178,11 @@ public class WorkForDictionary implements Runnable {
         while (bl){
             System.out.print("\nВведите английское слово: ");
             String str = scan.nextLine();
-            System.out.printf("\nСлово - '%s' имеет переводы:\n", str);
-            ArrayList<String> worlds = this.dict.dictionary.get(str);
-            for (String st : worlds) {
-                System.out.println("\t\t" + st);
+            for(World w: this.dict.worlds){
+                if(Objects.equals(w.name, str))
+                    w.count++;
             }
+            print(str, this.dict.dictionary.get(str));
             System.out.print("\nХотите просмотреть еще одно слово (1-Да, 2-Нет): ");
             int res = Integer.parseInt(scan.nextLine());
             switch (res) {
@@ -105,6 +191,14 @@ public class WorkForDictionary implements Runnable {
                 case 2 -> bl = false;
                 default -> throw new IllegalStateException("Unexpected value: " + res);
             }
+        }
+
+    }
+
+    private void print(String str, ArrayList<String> worlds){
+        System.out.printf("\nСлово - '%s' имеет переводы:\n", str);
+        for (String st : worlds) {
+            System.out.println("\t\t" + st);
         }
 
     }
